635063c9de51bb238fd0d753ec46bcc92a953fac
dodders <george.dodwell@gmail.com>
2017-09-03 19:41:59 -0400

    v2

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-require('fetch')
 var request = require('request');
 
 console.log('starting...')
 
 request('http://74.208.159.205:5000/Sensors', function(err, resp, body) {
-	console.log(err);
-	console.log(resp);
-	console.log(body);
+	//console.log(err);
+	//console.log(resp);
+    console.log('got response...');
+    var sensorData = JSON.parse(body)
+    console.log('items:', sensorData._items.length)
 });
 
 /*fetch('http://74.208.159.205:5000/Sensors').then(function(resp) {
