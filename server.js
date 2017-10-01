9939b8c9796f3d6133a46ba7c4e074e76d8c1dc4
George Dodwell <george.dodwell@gmail.com>
2017-10-01 15:26:52 -0400

    multi-page handling and date selection

--- server.js
+++ server.js
@@ -3,21 +3,36 @@ var dateformat = require('dateformat')
 
 console.log('starting...')
 
-var url = 'http://74.208.159.205:5000/Sensors?where={"type":"F", "node_id":"41"}'
+var oldurl = 'http://74.208.159.205:5000/Sensors?where={"type":"F","node_id":"41"}'
+//url2 is hopefully 7/23/2017 9am to 7/23/2017 10am.
+var urlbase = 'http://74.208.159.205:5000/Sensors?page='
+var urlparams = '&where={"type":"F","node_id":"41","time":{"$gte":1500814800},"time":{"$lte":1500818400}}'
+var url2 = 'http://74.208.159.205:5000/Sensors?page=1&where={"type":"F","node_id":"41","time":{"$gte":1500800400},"time":{"$lte":1500804000}}'
 //var url = 'http://74.208.159.205:5000/Sensors' 
 
-request(url, function(err, resp, body) {
-	//console.log(err);
-	//console.log(resp);
-    console.log('body retrieved! processing...')
-    var sensorData = JSON.parse(body)
-    console.log(sensorData._items.length, ' items found.')
-    var curData = [] //array of objects {'date': dateObj, 'value': valueObj}
-    sensorData._items.forEach(function(el) {
-        curData.push({'date': fmtDate(el.time), 'value': fmtTemp(el.value)});
-    }, this);
-    console.log(curData)
-});
+//just do this! so odd...
+getPage(1)
+
+function getPage(currpage) {
+    var url = urlbase.concat(currpage).concat(urlparams)
+    console.log('retrieving url:', url)
+    request(url, function(err, resp, body) {
+        console.log('body retrieved! processing...')
+        var sensorData = JSON.parse(body)
+        console.log(sensorData._items.length, ' items found.')
+        var curData = [] //array of objects {'date': dateObj, 'value': valueObj}
+        sensorData._items.forEach(function(el) {
+            curData.push({'date': fmtDate(el.time), 'value': fmtTemp(el.value)});
+        }, this)
+        console.log('page ', currpage, ':', curData)
+
+        if (sensorData._links.next != null) {
+            getPage(currpage + 1)
+        } else {
+            console.log('no more pages to retrieve.')
+        }
+    });
+}
 
 function fmtDate(epochtime) {
     var d = new Date(epochtime * 1000)
@@ -27,18 +42,3 @@ function fmtDate(epochtime) {
 function fmtTemp(temp) {
     return temp.replace("\'","").replace("\'","").replace('b','').trim()    
 }
-
-/*fetch('http://74.208.159.205:5000/Sensors').then(function(resp) {
-	console.log(resp)
-});*/
-
-/*ax({
-    method: 'get',
-    //url: 'https://jsonplaceholder.typicode.com/posts/1'
-    url: 'http://74.208.159.205:5000/Sensors'
-})
-    .then(function(resp){
-        console.log(resp.data._meta)
-    })*/
-
-
